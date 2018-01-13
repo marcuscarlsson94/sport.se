0a227a462def50cc45ba1aab465a3f0fad14680c
Marcus Carlsson <marcuscarlsson@Marcuss-iMac.lan>
2018-01-13 19:57:44 +0100

    added fixed header on scroll

---
+++ js/sticky-header.js
@@ -0,0 +1,21 @@
+// var $document = $(document),
+//     $element = $('header'),
+//     className = 'hasScrolled';
+//
+// $document.scroll(function() {
+//   if ($document.scrollTop() >= 150) {
+//     // user scrolled 150 pixels or more;
+//     // do stuff
+//     $element.addClass(fixed);
+//   } else {
+//     $element.removeClass(fixed);
+//   }
+// });
+
+var $document = $(document),
+    $element = $('header'),
+    className = 'fixed';
+
+$document.scroll(function() {
+  $element.toggleClass(className, $document.scrollTop() >= 150);
+});
