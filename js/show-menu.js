5d3625a0aa96086b14eceaae5473805ecc6bff58
Marcus Carlsson <marcuscarlsson@Marcuss-iMac.lan>
2018-01-13 16:38:46 +0100

    working on a push nav

---
+++ js/show-menu.js
@@ -0,0 +1,26 @@
+// $('hamburger').click(function(){
+//         $('body').addClass('slide-left');
+// });
+
+$(function() {
+        $(".hamburger").click(function() {
+            $("main").toggleClass('slide-left');
+        });
+
+    });
+
+$(function() {
+        if $(".hamburger").click(function() {
+            $("main").toggleClass('slide-left');
+        });
+
+    });
+
+
+
+// $(function() {
+//         if $('.hamburger').hasClass(".change") { .click(function() {
+//             $("body").removeClass('slide-left');
+//         });
+//
+//     });
