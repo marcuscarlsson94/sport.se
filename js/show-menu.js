68f4293f292cc58f050d182b01160a6a9fc8dec5
Marcus Carlsson <marcuscarlsson@Marcuss-iMac.lan>
2018-01-14 15:41:24 +0100

    working on making MENYtoggle between MENY and STÄNG

--- js/show-menu.js
+++ js/show-menu.js
@@ -5,9 +5,23 @@
 $(function() {
         $(".hamburger").click(function() {
             $("main").toggleClass('slide-left');
+            if(!$(this).next('.change').is(":visible") )
+            $('.swap').text('STÄNG');
+            else if (!$(this).next('.change').is(":hidden") )
+            $('.swap').text('MENY');
         });
-
     });
+
+
+
+
+    // $('.swap').toggle(function(){
+    //    $(this).text('MENY');
+    //   },
+    //  function(){
+    //    $(this).text('STÄNG');
+    //  }
+    // );
 
 
 
