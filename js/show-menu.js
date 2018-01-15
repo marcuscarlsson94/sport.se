927e5a747b3d00f8a53db63d2ee2aaad11cbe4e6
Marcus Karlsson <marcuskarlsson@Maxs-MacBook-Pro.local>
2018-01-15 12:51:19 +0100

    -mchanged the nav to work on all sizes

--- js/show-menu.js
+++ js/show-menu.js
@@ -1,45 +1,9 @@
-// $('hamburger').click(function(){
-//         $('body').addClass('slide-left');
-// });
-
 $(function() {
         $(".hamburger").click(function() {
             $("main").toggleClass('slide-left');
-            if(!$(this).hasClass("change") )
-            $('.swap').text('MENY');
-            else
+            if($(this).hasClass("change") )
             $('.swap').text('STÄNG');
+            else
+            $('.swap').text('MENY');
         });
     });
-
-
-
-// $(function() {
-//         $(".hamburger").click(function() {
-//             $("main").toggleClass('slide-left');
-//             if(!$(this).next('.hamburger').hasClass("change") )
-//             $('.swap').text('STÄNG');
-//             else if (!$(this).next('.hamburger').not(":hidden") )
-//             $('.swap').text('MENY');
-//         });
-//     });
-
-
-
-
-    // $('.swap').toggle(function(){
-    //    $(this).text('MENY');
-    //   },
-    //  function(){
-    //    $(this).text('STÄNG');
-    //  }
-    // );
-
-
-
-// $(function() {
-//         if $('.hamburger').hasClass(".change") { .click(function() {
-//             $("body").removeClass('slide-left');
-//         });
-//
-//     });
